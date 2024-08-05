09567c9bbba0cb92e699551e37bc0001e439a491
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2024-08-05 16:20:26 -0300

    Adding files

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# cs50-project
+# BUILDATA
+#### Video Demo:  <URL [https://youtu.be/RDXGWPb5yQI?si=aLBf_D-08Pa3KCFo]>
+#### Description:
+
+**Motivation**
+
+Buildata is a Web Application service that facilitates all managing processes throughout a construction. Users are able to see and organize all data related to their constructions, including their workers as well. The platform was developed specially to make an easy-access connection between construction's managers and their clients, also to centralize all data a manager must handle, in a single interface.
+
+**Functionalities**
+
+Diving into Buildata's ecosystem, we are able to explore some features, linked with some pages as well. Once users are logged into the platform, there's a homepage containing all indepent features user can use.
+* Workers: Users can register and delete workers from their accounts. When the first worker is registered, a table is created in the Manage workers page, showing workers' data. If there is no worker registered, a blue button will be shown right in the middle of the screen, leading users to register their first worker.
+* Constructions: After the first worker's registration, the user can add a new construction to their account, specifying some construction's data, such as a name for it, the client's name, where's the construction's location - its address - and the duration of the process. After the first construction registration, at the Manage constructions page, there will be a card for each construction registered, containing its main specifications. Those cards are actually buttons, leading the user to a speciffic page with some more funcionalities.
+* Construction's information page: When a card is clicked, that page will be brought on screen. In a simple and minimalistic interface, the page displays the main information about the construction at stake and show the responsible staff team as well.
+* Posting updates: In addition, the user, as a manager, is able to update their clients about the constructions' progress, by posting all the modifications done during a certain time interval. Posts are a easy way of shorten the distance between a manager and their clients, by setting a continuos update connection, since clients can easily access all stuff done in their houses, appartments, etc, without the need to make frequent visits. Also followed by the possibility of seeing photos of those updates (up to the manager posting it or not).
+
+**Technologies Used**
+
+To make this idea possible, I had to put some technologies learned during the course together:
+* Front-end: To create all the web interface, I have used HTML, CSS and even JS to help in some input formatting. In addition, the template Jinja2 was also used to simplify page structures and make the page's building process faster.
+* Back-end: To make all logic operations and the integration of web structure and database, I have used, in Python, Flask library, and the SQL library from CS50 as well.
+* Database: To store server's data, there is a SQLite database, containing the following tables:
+    * users: Containing users' data, such as id, username and password (hash)
+    * workers: Containing workers' data, and also a Users FK*
+    * constructions: Containing constructions' data, such as their id, Users FK, their name, client name, address, starting and ending dates
+    * selected_workers: Responsible for linking, when selected into the platform, a worker to a construction, meaning that worker will be in the staff team of the created construction
+    * posts: Containing posts' data, such as id, Users FK, Constructions FK, the posting datetime, and the post information (Title and content)
+    * photos: Will contain, not the photo bytes by themselves, but a file path to them. At the moment the user submits a post with photos, the script will save those photos to the local repository, in this case, cs50 Codespace. It works that way to ensure the photos' byte-content will not damage the integrity of the database, in case of multiple high-quality photos uploading, for example.
+
+**_OBS:_** _something_ FK refers to a Foreign Key used to link the respective table with the _something_ table. Example: Users FK - refering that the topic table has a Foreign Key to the users table.
+
+**Instructions**
+
+Here are some instructions to facilitate the platform's flow during the usage:
+* User registration system: The first screen shows a log in/sign in system, and that actually works! If you are not registered yet, you must create a new account, putting a username and choosing a password. Right after the account creation, you will be automatically redirected to the platform, not being necessary putting your data again just to log in. If you log out and want to return to the same account, you must put the exact same information (username and password) as before during the registration, but, this time, at the log in page.
+* Construction registration: When adding a new construction, you MUST have at least one worker registered, otherwise, the application will send you an error message, advertising the missing data
+* Worker deleting: If you delete a worker from your workers' table, he will be automatically deleted from all constructions attached to him.
+* Selecting the same worker in the staff team: When adding a new construction, the staff interface allows you to select the same worker in different inputs. Although it is not ideal, if it occures, the application will just "group" data with the same values to insert into the database, so the selected workers links will be made once per worker's name.
---
+++ app.py
@@ -0,0 +1,381 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+
+from datetime import datetime
+
+# Create flask application
+app = Flask(__name__)
+
+# Session to be filesystem not cookies
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# Initialize SQL database by cs50 library
+db = SQL("sqlite:///buildata.db")
+
+
+# Ensure user is logged in
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Rendering the home page, with user's name on it
+@app.route("/")
+@login_required
+def index():
+    username = db.execute("SELECT username FROM users WHERE id=?", session["user_id"])
+    return render_template("home_page.html", name=username[0])
+
+
+# Worker's table management
+@app.route("/mng_workers")
+@login_required
+def mng_workers():
+    data_workers = db.execute("SELECT * FROM workers WHERE user_id=?", session["user_id"])
+    return render_template("mng_workers.html", data_workers=data_workers)
+
+
+# Registering user in platform
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+
+    # In case of submitting sign in form
+    if request.method == "POST":
+
+        # Gets all users usernames
+        data_users = db.execute("SELECT * FROM users")
+
+        # Getting user input from .html
+        data_input = {
+            "username": request.form.get("username"),
+            "password": request.form.get("password"),
+            "password_confirmation": request.form.get("password_confirm")
+        }
+
+        # Ensure all inputs are fulfilled
+        for data in data_input.values():
+            if data == '':
+                return render_template("error.html", code="401", error_message="Must provide all inputs")
+
+        # Password and confirmation don't match
+        if data_input["password"] != data_input["password_confirmation"]:
+            return render_template("error.html", code="401", error_message="Passwords don't match")
+
+        # Checking if the username is already taken by another user
+        is_already_signed = False
+        for data in data_users:
+            if data["username"] == data_input["username"]:
+                is_already_signed = True
+
+        # If the username is not being used
+        if not is_already_signed:
+
+            # Hash the password (cryptography)
+            hash_password = generate_password_hash(data_input["password"])
+
+            # Insert new user data in database
+            db.execute("INSERT INTO users (username, password) VALUES (?, ?)", data_input["username"], hash_password)
+
+            # Set session ID to be user ID
+            user_id = db.execute("SELECT id FROM users WHERE username=?", data_input["username"])
+            session["user_id"] = user_id[0]["id"]
+
+            # Automatic login
+            flash(f"Welcome to BuilData user {data_input['username']}!")
+            return redirect("/")
+
+        # Username already taken
+        else:
+            return render_template("error.html", code="401", error_message="Username already taken")
+
+    # In case of just opening the section
+    else:
+        # If user returns to sign page after signing in
+        session.clear()
+
+        return render_template("signin.html")
+
+
+# Logging user
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        # Just being sure the session is clear
+        session.clear()
+
+        # Get user input from .html
+        data_input = {
+            "username": request.form.get("username"),
+            "password": request.form.get("password")
+        }
+
+        # Catch username data from database
+        data_users = db.execute("SELECT * FROM users WHERE username=?", data_input["username"])
+
+        # Ensure all inputs are fulfilled
+        for data in data_input.values():
+            if data == '':
+                return render_template("error.html", code="401", error_message="Must fill all inputs")
+
+        # Username input does not exist
+        if len(data_users) != 1:
+            return render_template("error.html", code="401", error_message="Username does not exist")
+
+        # Password from input and from database don't match
+        elif not check_password_hash(data_users[0]["password"], data_input["password"]):
+            return render_template("error.html", code="401", error_message="Incorrect password")
+
+        # Set session ID to be user ID
+        session["user_id"] = data_users[0]["id"]
+
+        # Get into page
+        flash(f"Welcome back to BuilData user {data_input['username']}!")
+        return redirect("/")
+
+    # In case of just opening the section
+    else:
+        # If user returns to login page after logging in
+        session.clear()
+
+        return render_template("login.html")
+
+
+# Register a new construction
+@app.route("/new_constr", methods=["GET", "POST"])
+@login_required
+def new_constr():
+    if request.method == "POST":
+        # Catch all user input
+        data_input = {
+            "constr-name": request.form.get("constr-name"),
+            "client-name": request.form.get("client-name"),
+            "client-address": request.form.get("client-address"),
+            "start-date": request.form.get("start-date"),
+            "end-date": request.form.get("end-date"),
+            "selected-workers": request.form.get("selected-workers")
+        }
+
+        # Check if all data was provided
+        for data in data_input.values():
+            if data == '':
+                return render_template("error.html", code="401", error_message="Must provide all construction data")
+
+        # Turn selected-workers data (csv) into a list
+        selected_workers = data_input["selected-workers"].split(",")
+        # Remove duplicates
+        selected_workers = list(dict.fromkeys(selected_workers))
+
+        # Insert construction data into database
+        db.execute("INSERT INTO constructions (user_id, construction_name, client, address, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?)",
+                   session["user_id"], data_input["constr-name"], data_input["client-name"], data_input["client-address"], data_input["start-date"], data_input["end-date"])
+
+        # Select construction id previously added
+        construction_id = db.execute("SELECT id FROM constructions WHERE user_id=? AND client=? AND construction_name=?",
+                                     session["user_id"], data_input["client-name"], data_input["constr-name"])
+
+        # For each worker in the workers' list
+        for worker in selected_workers:
+
+            # In case of blank inputs
+            if worker == '':
+                continue
+            # Search selected worker's id from workers' table
+            worker_id = db.execute("SELECT id FROM workers WHERE user_id=? AND name=?", session["user_id"], worker)
+
+            # Insert catched data into database
+            db.execute("INSERT INTO selected_workers (construction_id, worker_id) VALUES (?, ?)",
+                       construction_id[0]["id"], worker_id[0]["id"])
+
+        # Redirect user to construction's manager page
+        flash("Construction added successfully!")
+        return redirect("/mng_constr")
+    else:
+        # Display new contruction interface with all workers availables in each select
+        workers_data = db.execute("SELECT * FROM workers WHERE user_id=?", session["user_id"])
+        return render_template("new_constr.html", workers=workers_data)
+
+
+# Manage and show constructions created
+@app.route("/mng_constr")
+@login_required
+def mng_constr():
+    constructions = db.execute("SELECT * FROM constructions WHERE user_id=?", session["user_id"])
+    return render_template("mng_constr.html", constructions=constructions)
+
+
+# Retrive a specific construction's information
+@app.route("/constr_info", methods=["GET", "POST"])
+@login_required
+def constr_info():
+    # In case of user posting a contruction's update
+    if request.method == "POST":
+
+        # Keep construction's id selected
+        construction_id = session["constr_id"]
+        data_input = {
+            "title": request.form.get("post-title"),
+            "description": request.form.get("post-description"),
+            "photo": request.files.getlist("photos-uploaded")
+        }
+
+        # Record the exact time that the post was posted
+        date_time = datetime.now()
+        # Format the posting date
+        post_date = f"{date_time.strftime('%x')} | {date_time.strftime('%X')}"
+
+        # Insert into database
+        db.execute("INSERT INTO posts (user_id, construction_id, datetime, title, description) VALUES (?, ?, ?, ?, ?)",
+                   session["user_id"], construction_id, post_date, data_input["title"], data_input["description"])
+
+        # Checking if at least one photo was uploaded
+        if data_input["photo"] is not None and len(data_input["photo"]) != 0 and data_input["photo"][0].filename != "":
+
+            # In case of user uploading multiple photos for one post, we need to save them individually
+            for photo in data_input["photo"]:
+                # Catch the post id just submitted
+                new_post_id = db.execute("SELECT id FROM posts WHERE datetime=?", post_date)
+
+                # Folders paths
+                user = db.execute("SELECT username FROM users WHERE id=?", session["user_id"])[0]["username"]
+                client = db.execute("SELECT client FROM constructions WHERE id=?", construction_id)[0]["client"]
+
+                photo_path = f"static/images/{user}/{client}/"
+
+                if not os.path.exists(photo_path):
+                    os.makedirs(photo_path)
+
+                # Saving the photo
+                photo_path += photo.filename
+                photo.save(photo_path)
+
+                # Inserting photo into database (related with it's post)
+                db.execute("INSERT INTO photos (post_id, photo) VALUES (?, ?)", new_post_id[0]["id"], photo_path)
+
+        # Redirect user to the same page, but in a get request, so the page will be reloaded with actual info
+        flash(f"Post uploaded successfully!")
+        return redirect("/constr_info")
+    else:
+        # Get the construction's id from the mng_constr screen
+        construction_id = request.args.get("id")
+
+        # Ensure that the user is picking a construction to look at than just reloading the page
+        if construction_id:
+            session["constr_id"] = construction_id
+
+        # Data that will be displayed in screen
+        construction_data = db.execute("SELECT * FROM constructions WHERE id=?", session["constr_id"])
+        posts = db.execute("SELECT * FROM posts WHERE construction_id=? ORDER BY id DESC", session["constr_id"])
+        username = db.execute("SELECT username FROM users WHERE id=?", session["user_id"])
+        photos = db.execute("SELECT * FROM photos")
+
+        # Formatting line breaks
+        for p in range(len(posts)):
+            posts[p]["description"] = posts[p]["description"].replace("\r\n", "<br>")
+
+        # Staff team
+        selected_workers = db.execute("""SELECT * FROM workers
+                           JOIN selected_workers ON workers.id = worker_id
+                           JOIN constructions ON selected_workers.construction_id = constructions.id
+                           WHERE workers.user_id=? AND construction_id=?""", session["user_id"], session["constr_id"])
+
+        return render_template("constr_info.html", construction_data=construction_data[0], posts=posts, photos=photos, username=username[0]["username"], selected_workers=selected_workers)
+
+
+# Registering a worker into the database
+@app.route("/register_worker", methods=["GET", "POST"])
+@login_required
+def register_worker():
+    if request.method == "POST":
+
+        # Get user input from .html
+        data_input = {
+            "name": request.form.get("name"),
+            "role": request.form.get("role"),
+            "phone": request.form.get("phone"),
+            "email": request.form.get("email")
+        }
+
+        # Ensure all inputs are fulfilled
+        for data in data_input.values():
+            if data == '':
+                return render_template("error.html", code="401", error_message="Must fill all inputs")
+
+        # Select all workers registered in user's account
+        data_users = db.execute("SELECT name FROM workers WHERE user_id=?", session["user_id"])
+
+        # In case of worker's name already exists in database
+        for data in data_users:
+            if data["name"].lower() == data_input["name"].lower():
+                return render_template("error.html", code="401", error_message="Worker is already registered")
+
+        # Insert worker's data into database
+        db.execute("INSERT INTO workers (user_id, name, role, phone, email) VALUES (?, ?, ?, ?, ?)",
+                   session["user_id"], data_input["name"].title(), data_input["role"], data_input["phone"], data_input["email"])
+
+        # Return to the manage section
+        flash(f"Worker {data_input['name'].title()} registered!")
+        return redirect("/mng_workers")
+
+    # In case of just opening the section
+    else:
+        return render_template("register_worker.html")
+
+
+# Deleting (firing) a worker from the database
+@app.route("/delete_worker", methods=["GET", "POST"])
+@login_required
+def delete_worker():
+
+    # Catch worker's data assigned to user's account
+    data_users = db.execute("SELECT * FROM workers WHERE user_id=?", session["user_id"])
+
+    if request.method == "POST":
+        # Querying worker's name
+        worker = request.form.get("worker")
+
+        # Deleting worker's data from database based on their name
+        db.execute("DELETE FROM selected_workers WHERE worker_id IN (SELECT id FROM workers WHERE name=?)", worker)
+
+        db.execute("DELETE FROM workers WHERE user_id=? AND name=?", session["user_id"], worker)
+
+        # Return to manage section
+        flash(f"Worker {worker} deleted")
+        return redirect("/mng_workers")
+
+    # In case of just opening the section where
+    else:
+        return render_template("delete_worker.html", workers=data_users)
+
+
+# In case user wants to log out
+@app.route("/logout")
+@login_required
+def logout():
+    # Clearing the session
+    session.clear()
+
+    # Return to home section -> is not logged -> log in page
+    flash("User logged out.")
+    return redirect("/")
